a51745b4725283633b5905124587960df8598353
Athul <athuldevan90@gmail.com>
2026-05-04 13:28:46 +0530

    fix:cors issue

--- src/index.ts
+++ src/index.ts
@@ -16,29 +16,30 @@ if (!db_url) {
   console.error("DB_URL not defined in env.");
   process.exit(1);
 }
+
 mongoose
   .connect(db_url)
   .then(() => console.info("Connected To MongoDb"))
   .catch((e) => console.error("Failed to Connect", e.stack));
 
-app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: "https://doctor-booking-application-client.vercel.app",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/token", tokenRoutes);
-
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/slots", slotRoutes);
 app.use("/api/admin", adminRoutes);
 
+// Error Handling
 app.use(errorHandler);
 
 export default app;
